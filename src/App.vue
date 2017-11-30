3919fa87f943120ecb6f85c52aba03fe6966de7e
Francis Yang <xyang42@ucsc.edu>
2017-11-29 18:27:24 -0800

    Added Todo component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,33 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <!--<div id="app">-->
+    <!--<img src="./assets/logo.png">-->
+    <!--<router-view/>-->
+  <!--</div>-->
+  <div>
+    <todo-list></todo-list>
   </div>
 </template>
 
 <script>
+  // eslint-disable-next-line
+  /* eslint-disable */
+  import TodoList from './components/TodoList';
+
 export default {
   name: 'app',
+  components: {
+    TodoList,
+  },
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/*#app {*/
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*text-align: center;*/
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
+/*}*/
 </style>
